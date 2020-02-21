309f67ca055ef9a20d9054c3511a9d41b32a850f
ionut.vacariu <ionut.vacariu123@gmail.com>
2020-02-21 14:27:58 +0200

    [VI] - kafka consumer - from hardcoded ip to localhost

--- src/main/java/kafka/IKafkaConstants.java
+++ src/main/java/kafka/IKafkaConstants.java
@@ -1,7 +1,9 @@
 package kafka;
 
 public interface IKafkaConstants {
-    public static String KAFKA_BROKERS = "172.16.233.98:9092";
+    // public static String KAFKA_BROKERS = "172.16.233.98:9092";
+
+    public static String KAFKA_BROKERS = "localhost:9092";
     public static Integer MESSAGE_COUNT = 1000;
     public static String CLIENT_ID = "client1";
     public static String TOPIC_NAME = "testing";
